0dd0681a803bff6e064e129dd836e72ee83e2112
Hakam-aldeen-Kh <hakamha8@gmail.com>
2024-06-05 13:11:30 +0300

    header & edit media queries

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,44 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import logo from "../assets/svg/logo.svg";
 function Header() {
   const [links] = useState(["Futures", "Team", "SignIn"]);
+  const headerRef = useRef<HTMLElement | null>(null);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100 && headerRef.current) {
+        headerRef.current.style.background = "#0c1524";
+        headerRef.current.style.padding = "20px 0px";
+      }
+      else  if(headerRef.current) {
+        headerRef.current.style.background = "transparent";
+        headerRef.current.style.paddingTop = "60px";
+      }
+    });
+  }, []);
   return (
-    <header>
-      <a href="#">
-        <img src={logo} />
-      </a>
-      <nav>
-        <ul className="flex items-center gap-[50px]">
-          {links.map((link: string) => (
-            <li>
-              <a
-                className="text-white opacity-[0.9] hover:opacity-[1] hover:underline transition-opacity duration-200"
-                href={`/${link.toLowerCase()}`}
-              >
-                {link}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <header
+      ref={headerRef}
+      className="pt-[60px] fixed top-0 left-0 w-full z-50 transition-all duration-200"
+    >
+      <div className="container px-[60px] mx-auto flex justify-between items-center gap-[30px] sm:gap-0  flex-col sm:flex-row">
+        <a href="#">
+          <img src={logo} />
+        </a>
+        <nav>
+          <ul className="flex items-center gap-[50px]">
+            {links.map((link: string) => (
+              <li>
+                <a
+                  className="text-white opacity-[0.9] hover:opacity-[1] hover:underline transition-opacity duration-200"
+                  href={`/${link.toLowerCase()}`}
+                >
+                  {link}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
