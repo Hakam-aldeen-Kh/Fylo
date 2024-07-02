fc1772b75749616e5037f15b44ae2e29181c888b
Hakam-aldeen-Kh <hakamha8@gmail.com>
2024-07-02 23:41:36 +0300

    fix problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-
+import { Button } from "@material-tailwind/react";
 export default function App() {
   return (
     <>
       <Header />
       <Home />
+      <Button className="bg-red-400">Button</Button>
       <Footer />
     </>
   );
